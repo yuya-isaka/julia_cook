2e4a60ec452f4c1dfcb5de030b8282ab8aba1ba9
yuya-isaka <zxwg6329@gmail.com>
2020-08-31 18:10:22 +0900

    いろいろ

--- sample.jl
+++ sample.jl
@@ -1,20 +1,20 @@
 
-# file = open("a.txt", "w")
-# count = 0
-# for i in 0:1200
-#     tmp = "ring_xnor_p2 ro$i (.start(ro_mode[$count]), .clk_s(clk_in[$count]));\n"
-#     write(file, tmp)
-#     global count += 2
-# end
-# close(file)
+file = open("a.txt", "w")
+count = 2
+for i in 0:400
+    tmp = "ring_xnor_p2 ro$i (.start(ro_mode[$count]), .clk_s(clk_in[$count]));\n"
+    write(file, tmp)
+    global count += 2
+end
+close(file)
 
 file = open("b.txt", "w")
-count = 0
-count2 = 31
-for i in 0:450
+count = 2
+count2 = 7231
+for i in 0:225
     tmp = "counter counter$i(.clk(clk_out[$count]), .rst(fsa), .c1000(Q[$count2:$(count2-31)]));\n"
     write(file, tmp)
     global count += 2
-    global count2 += 32
+    global count2 -= 32
 end
 close(file)
