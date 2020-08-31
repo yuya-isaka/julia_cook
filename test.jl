d56d67ff761b1cc40bb7ef7ff0ccfdbfaa9968d5
yuya-isaka <zxwg6329@gmail.com>
2020-08-31 14:13:15 +0900

    fix $便利 あとor文はpythonと違って末尾も含まれる（直感的）,global指定する

--- test.jl
+++ test.jl
@@ -2,7 +2,8 @@
 
 count = 319
 for i in 0:9
-    tmp = "ring_counter_uart ro$(i+1)(.ro_mode(ro_mode[$i]), .clk_in(clk_in[$i]), .clk_out(clk_out), .fsa(fsa), .q(q[$count:$(count-31)]));"
+    # tmp = "ring_counter_uart ro$(i+1)(.ro_mode(ro_mode[$i]), .fsa(fsa), .q(q[$count:$(count-31)]));"
+    tmp = "ring_counter_uart counter$i(.romode(romode[$i]), .fsa(fsa), .Q(Q[$count:$(count-31)]));"
     println(tmp)
     global count -= 32
 end
