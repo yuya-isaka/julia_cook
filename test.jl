3eb38db1ff560ea3e96f13f1ad5c3ca01e6dd74f
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2020-08-31 10:23:40 +0900

    )のつけ忘れ

--- test.jl
+++ test.jl
@@ -2,7 +2,7 @@
 
 count = 319
 for i in 0:9
-    tmp = "ring_counter_uart ro$(i+1)(.ro_mode(ro_mode[$i], .clk_in(clk_in[$i]), .clk_out(clk_out), .fsa(fsa), .q(q[$count:$(count-31)]));"
+    tmp = "ring_counter_uart ro$(i+1)(.ro_mode(ro_mode[$i]), .clk_in(clk_in[$i]), .clk_out(clk_out), .fsa(fsa), .q(q[$count:$(count-31)]));"
     println(tmp)
     global count -= 32
 end
