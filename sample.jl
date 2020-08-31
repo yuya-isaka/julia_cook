a66e8784438c6eeee7ed3be8087a0ddfe8138ed4
yuya-isaka <zxwg6329@gmail.com>
2020-08-31 20:29:50 +0900

    fix

--- sample.jl
+++ sample.jl
@@ -1,7 +1,7 @@
 
 file = open("a.txt", "w")
 count = 2
-for i in 0:400
+for i in 0:100
     tmp = "ring_xnor_p2 ro$i (.start(ro_mode[$count]), .clk_s(clk_in[$count]));\n"
     write(file, tmp)
     global count += 2
@@ -10,8 +10,8 @@ close(file)
 
 file = open("b.txt", "w")
 count = 2
-count2 = 7231
-for i in 0:225
+count2 = 3231
+for i in 0:100
     tmp = "counter counter$i(.clk(clk_out[$count]), .rst(fsa), .c1000(Q[$count2:$(count2-31)]));\n"
     write(file, tmp)
     global count += 2
