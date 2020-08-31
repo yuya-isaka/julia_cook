d6fa6f1d4d0dc8f350d2084f2367b7212ea217c5
yuya-isaka <zxwg6329@gmail.com>
2020-08-31 15:33:58 +0900

    fix

--- test.jl
+++ test.jl
@@ -1,9 +1,11 @@
 
 
 count = 319
+tmp = 2
 for i in 0:9
     # tmp = "ring_counter_uart ro$(i+1)(.ro_mode(ro_mode[$i]), .fsa(fsa), .q(q[$count:$(count-31)]));"
-    tmp = "ring_counter_uart counter$i(.romode(romode[$i]), .fsa(fsa), .Q(Q[$count:$(count-31)]));"
-    println(tmp)
+    a = "ring_counter_uart counter$i(.ro_mode(ro_mode[$tmp]), .fsa(fsa), .Q(Q[$count:$(count-31)]));"
+    println(a)
     global count -= 32
+    global tmp += 2
 end
