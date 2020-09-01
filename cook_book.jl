6ae8ced5686b82e113aa4c49a076c6a83fcf04b1
yuya-isaka <zxwg6329@gmail.com>
2020-09-01 11:34:40 +0900

    fix

--- cook_book.jl
+++ cook_book.jl
@@ -1,14 +1,10 @@
-x = []
-x = Int[]
-x = Vector{Int}()
-x = [1,2,3]
-
-a = fill(42,10)
-print(a)
-
-b = fill("foo", 4)
-println(b)
-
-x = [1,2,3]
-push!(x,4)
-println(x)
+file = open("c.txt", "w")
+count = 2
+q = 351
+for i in 0:10
+    tmp = "ring_xnor_p2 ro$i(.start(ro_mode[$count]), .rst(fsa), .c1000(Q[$q:$(q-31)]));\n"
+    write(file, tmp)
+    global count += 2
+    global q -= 32
+end
+close(file)
