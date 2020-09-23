05638e98b3d9d3ac8473799a6562c4062e8e5940
yuya-isaka <zxwg6329@gmail.com>
2020-09-23 16:18:11 +0900

    julia cook

--- cook_book.jl
+++ cook_book.jl
@@ -1,10 +1,8 @@
 file = open("c.txt", "w")
 count = 2
-q = 351
-for i in 0:10
-    tmp = "ring_xnor_p2 ro$i(.start(ro_mode[$count]), .rst(fsa), .c1000(Q[$q:$(q-31)]));\n"
+for i in 0:124
+    tmp = "ring_xor_new ro$i(.start(ro_mode[$count]), .clk_s(clk_in[$count]));\n"
     write(file, tmp)
     global count += 2
-    global q -= 32
 end
 close(file)
